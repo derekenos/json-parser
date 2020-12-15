2bfe71e605dc0eed1ee9c63fecd7959ad6f98e42
Derek Enos <derek.enos@gmail.com>
2020-12-15 09:50:06 -0500

    Consolidate all the inline string into new Matchers and Events classes.

--- jsonite.py
+++ jsonite.py
@@ -1,53 +1,80 @@
 
+###############################################################################
+# Exceptions
+###############################################################################
+
 class UnexpectedCharacter(Exception):
     def __init__(self, c, i):
         super().__init__(
             'Unexpected character "{}" at position {}'.format(c, i)
         )
 
-OBJECT_OPEN = b'{'
-ARRAY_OPEN = b'['
-STRING_START = b'"'
-STRING_TERMINATOR = b'"'
-NULL_START = b'n'
-IS_NUMBER_START = lambda c: c == b'-' or c.isdigit()
-OBJECT_KEY_START = STRING_START
-OBJECT_CLOSE = b'}'
-ARRAY_CLOSE = b']'
-KV_SEP = b':'
-ITEM_SEP = b','
-EOF = b''
-IS_NO_MATCH = lambda c: False
+###############################################################################
+# Matchers
+###############################################################################
 
-IS_VALUE_START = lambda c: (
-    c == OBJECT_OPEN
-    or c == ARRAY_OPEN
-    or c == STRING_START
-    or IS_NUMBER_START(c)
-    or c == NULL_START
+class Matchers:
+    OBJECT_OPEN = b'{'
+    ARRAY_OPEN = b'['
+    STRING_START = b'"'
+    STRING_TERMINATOR = b'"'
+    NULL_START = b'n'
+    IS_NUMBER_START = lambda c: c == b'-' or c.isdigit()
+    OBJECT_CLOSE = b'}'
+    ARRAY_CLOSE = b']'
+    KV_SEP = b':'
+    ITEM_SEP = b','
+    EOF = b''
+    IS_NO_MATCH = lambda c: False
+
+# Set derived matchers.
+Matchers.OBJECT_KEY_START = Matchers.STRING_START
+Matchers.IS_VALUE_START = lambda c: (
+    c == Matchers.OBJECT_OPEN
+    or c == Matchers.ARRAY_OPEN
+    or c == Matchers.STRING_START
+    or Matchers.IS_NUMBER_START(c)
+    or c == Matchers.NULL_START
 )
+Matchers.IS_OBJECT_VALUE_START = lambda c: Matchers.IS_VALUE_START(c)
+Matchers.IS_ARRAY_VALUE_START = lambda c: Matchers.IS_VALUE_START(c)
+Matchers.IS_NEXT_OBJECT_KEY_START = \
+    lambda c: c == Matchers.ITEM_SEP or c == Matchers.OBJECT_KEY_START
+Matchers.IS_NEXT_ARRAY_VALUE_START = \
+    lambda c: c == Matchers.ITEM_SEP or Matchers.IS_ARRAY_VALUE_START(c)
 
-IS_OBJECT_VALUE_START = lambda c: (
-    c == OBJECT_OPEN
-    or c == ARRAY_OPEN
-    or c == STRING_START
-    or IS_NUMBER_START(c)
-    or c == NULL_START
-)
+###############################################################################
+# Events
+###############################################################################
 
-IS_ARRAY_VALUE_START = lambda c: (
-    c == OBJECT_OPEN
-    or c == ARRAY_OPEN
-    or c == STRING_START
-    or IS_NUMBER_START(c)
-    or c == NULL_START
-)
+class Events:
+    OBJECT_OPEN = 'OBJECT_OPEN'
+    OBJECT_CLOSE = 'OBJECT_CLOSE'
+    ARRAY_OPEN = 'ARRAY_OPEN'
+    ARRAY_CLOSE = 'ARRAY_CLOSE'
+    OBJECT_KEY = 'OBJECT_KEY'
+    EOF = 'END_OF_FILE'
+    ARRAY_VALUE_STRING = 'ARRAY_VALUE_STRING'
+    ARRAY_VALUE_NUMBER = 'ARRAY_VALUE_NUMBER'
+    ARRAY_VALUE_NULL = 'ARRAY_VALUE_NULL'
+    OBJECT_VALUE_STRING = 'OBJECT_VALUE_STRING'
+    OBJECT_VALUE_NUMBER = 'OBJECT_VALUE_NUMBER'
+    OBJECT_VALUE_NULL = 'OBJECT_VALUE_NULL'
+    STRING = 'STRING'
+    NUMBER = 'NUMBER'
+    NULL = 'NULL'
 
-IS_NEXT_OBJECT_KEY_START = lambda c: c == ITEM_SEP or c == OBJECT_KEY_START
-IS_NEXT_ARRAY_VALUE_START = lambda c: c == ITEM_SEP or IS_ARRAY_VALUE_START(c)
+###############################################################################
+# Helpers
+###############################################################################
 
-is_not_string_terminator = lambda c: c != STRING_TERMINATOR
+is_not_string_terminator = lambda c: c != Matchers.STRING_TERMINATOR
 is_number_char = lambda c: c.isdigit() or c == b'.'
+
+
+###############################################################################
+# Parser
+###############################################################################
 
 class Parser:
     def __init__(self, stream):
@@ -72,8 +99,8 @@ class Parser:
         # the character, or None if the stream has been exhausted.
         while True:
             c = self.next_char()
-            if c == EOF:
-                return EOF
+            if c == Matchers.EOF:
+                return Matchers.EOF
             if not c.isspace():
                 return c
 
@@ -82,17 +109,18 @@ class Parser:
             raise AssertionError
         self.stuffed_char = c
 
-    def expect(self, expected):
+    def expect(self, matcher):
         # Assert that the next non-whitespace char is as expected and return
         # it.
         c = self.next_nonspace_char()
-        while isinstance(expected, tuple):
-            optional, expected = expected
-            if (not hasattr(optional, 'decode') and optional(c)) or c == optional:
-                self.expect_stack.append(expected)
+        while isinstance(matcher, tuple):
+            optional, matcher = matcher
+            if ((not hasattr(optional, 'decode') and optional(c))
+                or c == optional):
+                self.expect_stack.append(matcher)
                 return c, optional
-        if (not hasattr(expected, 'decode') and expected(c)) or c == expected:
-            return c, expected
+        if (not hasattr(matcher, 'decode') and matcher(c)) or c == matcher:
+            return c, matcher
         raise UnexpectedCharacter(c, self.char_num)
 
     def yield_while(self, pred):
@@ -118,14 +146,14 @@ class Parser:
 
     def parse_string(self):
         yield from self.yield_while(is_not_string_terminator)
-        self.expect(STRING_TERMINATOR)
+        self.expect(Matchers.STRING_TERMINATOR)
 
     def parse_number(self, first_char):
         yield first_char
         yield from self.yield_while(is_number_char)
 
     def parse(self):
-        self.expect_stack = [ EOF, IS_VALUE_START ]
+        self.expect_stack = [ Matchers.EOF, Matchers.IS_VALUE_START ]
         while True:
             for x in self.parse_next():
                 if x is None:
@@ -135,110 +163,142 @@ class Parser:
     def parse_next(self):
         c, match = self.expect(self.expect_stack.pop())
 
-        if match == EOF:
+        if match == Matchers.EOF:
             yield None
             return
 
-        if match == OBJECT_CLOSE:
-            yield 'OBJECT_CLOSE'
+        if match == Matchers.OBJECT_CLOSE:
+            yield Events.OBJECT_CLOSE
             return
 
-        if match == OBJECT_KEY_START:
-            yield from self.with_drain('OBJECT_KEY', self.parse_string())
-            self.expect_stack.append(KV_SEP)
+        if match == Matchers.OBJECT_KEY_START:
+            yield from self.with_drain(Events.OBJECT_KEY, self.parse_string())
+            self.expect_stack.append(Matchers.KV_SEP)
             return
 
-        if match == KV_SEP:
-            self.expect_stack.append(IS_OBJECT_VALUE_START)
+        if match == Matchers.KV_SEP:
+            self.expect_stack.append(Matchers.IS_OBJECT_VALUE_START)
             return
 
-        if match == ARRAY_CLOSE:
-            yield 'ARRAY_CLOSE'
+        if match == Matchers.ARRAY_CLOSE:
+            yield Events.ARRAY_CLOSE
             return
 
-        if match == IS_NEXT_OBJECT_KEY_START:
+        if match == Matchers.IS_NEXT_OBJECT_KEY_START:
             self.expect_stack.append(
                 (
-                    OBJECT_KEY_START,
-                    (IS_NEXT_OBJECT_KEY_START, self.expect_stack.pop())
+                    Matchers.OBJECT_KEY_START,
+                    (
+                        Matchers.IS_NEXT_OBJECT_KEY_START,
+                        self.expect_stack.pop()
+                    )
                 )
             )
             return
 
-        if match == IS_NEXT_ARRAY_VALUE_START:
+        if match == Matchers.IS_NEXT_ARRAY_VALUE_START:
             self.expect_stack.append(
                 (
-                    IS_ARRAY_VALUE_START,
-                    (IS_NEXT_ARRAY_VALUE_START, self.expect_stack.pop())
+                    Matchers.IS_ARRAY_VALUE_START,
+                    (
+                        Matchers.IS_NEXT_ARRAY_VALUE_START,
+                        self.expect_stack.pop()
+                    )
                 )
             )
             return
 
         # Assert that this is some sort of value match.
-        if match not in (IS_VALUE_START, IS_OBJECT_VALUE_START,
-                         IS_ARRAY_VALUE_START):
+        if match not in (
+                Matchers.IS_VALUE_START,
+                Matchers.IS_OBJECT_VALUE_START,
+                Matchers.IS_ARRAY_VALUE_START
+            ):
             raise AssertionError(c, match, self.char_num)
 
-        if c == OBJECT_OPEN:
-            yield 'OBJECT_OPEN'
+        if c == Matchers.OBJECT_OPEN:
+            yield Events.OBJECT_OPEN
             self.expect_stack.append(
                 (
-                    OBJECT_KEY_START,
-                    (IS_NEXT_OBJECT_KEY_START, OBJECT_CLOSE)
+                    Matchers.OBJECT_KEY_START,
+                    (
+                        Matchers.IS_NEXT_OBJECT_KEY_START,
+                        Matchers.OBJECT_CLOSE
+                    )
                 )
             )
             return
 
-        if c == ARRAY_OPEN:
-            yield 'ARRAY_OPEN'
+        if c == Matchers.ARRAY_OPEN:
+            yield Events.ARRAY_OPEN
             self.expect_stack.append(
                 (
-                    IS_ARRAY_VALUE_START,
-                    (IS_NEXT_ARRAY_VALUE_START, ARRAY_CLOSE)
+                    Matchers.IS_ARRAY_VALUE_START,
+                    (
+                        Matchers.IS_NEXT_ARRAY_VALUE_START,
+                        Matchers.ARRAY_CLOSE
+                    )
                 )
             )
             return
 
-        value_context = ''
-        if match == IS_OBJECT_VALUE_START:
-            value_context = 'OBJECT_VALUE_'
-        elif match == IS_ARRAY_VALUE_START:
-            value_context = 'ARRAY_VALUE_'
+        if c == Matchers.STRING_START:
+            if match == Matchers.IS_OBJECT_VALUE_START:
+                event = Events.OBJECT_VALUE_STRING
+            elif match == Matchers.IS_ARRAY_VALUE_START:
+                event = Events.ARRAY_VALUE_STRING
+            else:
+                event = Events.STRING
+            yield from self.with_drain(event, self.parse_string())
 
-        if c == STRING_START:
-            yield from self.with_drain(
-                '{}STRING'.format(value_context), self.parse_string()
-            )
-        elif IS_NUMBER_START(c):
-            yield from self.with_drain(
-                '{}NUMBER'.format(value_context), self.parse_number(c)
-            )
-        elif c == NULL_START:
+        elif Matchers.IS_NUMBER_START(c):
+            if match == Matchers.IS_OBJECT_VALUE_START:
+                event = Events.OBJECT_VALUE_NUMBER
+            elif match == Matchers.IS_ARRAY_VALUE_START:
+                event = Events.ARRAY_VALUE_NUMBER
+            else:
+                event = Events.NUMBER
+            yield from self.with_drain(event, self.parse_number(c))
+
+        elif c == Matchers.NULL_START:
             self.expect(b'u')
             self.expect(b'l')
             self.expect(b'l')
-            yield '{}NULL'.format(value_context)
+            if match == Matchers.IS_OBJECT_VALUE_START:
+                yield Events.OBJECT_VALUE_NULL
+            elif match == Matchers.IS_ARRAY_VALUE_START:
+                yield Events.ARRAY_VALUE_NULL
+            else:
+                yield Events.NULL
+
         else:
             raise NotImplementedError(c)
 
-        if match == IS_ARRAY_VALUE_START:
+        if match == Matchers.IS_ARRAY_VALUE_START:
             self.expect_stack.append(
-                (IS_NEXT_ARRAY_VALUE_START, self.expect_stack.pop())
+                (Matchers.IS_NEXT_ARRAY_VALUE_START, self.expect_stack.pop())
             )
-        elif match == IS_OBJECT_VALUE_START:
+        elif match == Matchers.IS_OBJECT_VALUE_START:
             self.expect_stack.append(
-                (IS_NEXT_OBJECT_KEY_START, self.expect_stack.pop())
+                (Matchers.IS_NEXT_OBJECT_KEY_START, self.expect_stack.pop())
             )
 
-    def convert(self, _type, value):
+    def convert(self, event, value):
         # Convert a parsed value to a Python type.
-        if _type.endswith('NULL'):
+        if (event == Events.ARRAY_VALUE_NULL
+            or event == Events.OBJECT_VALUE_NULL
+            or event == Events.NULL):
             return None
-        if _type.endswith('STRING') or _type == 'OBJECT_KEY':
+        if (event == Events.ARRAY_VALUE_STRING
+            or event == Events.OBJECT_VALUE_STRING
+            or event == Events.STRING
+            or event == Events.OBJECT_KEY):
             return b''.join(value)
-        if _type.endswith('NUMBER'):
+        if (event == Events.ARRAY_VALUE_NUMBER
+            or event == Events.OBJECT_VALUE_NUMBER
+            or event == Events.NUMBER):
             return float(b''.join(value))
-        raise NotImplementedError(_type, value)
+        raise NotImplementedError(event, value)
 
     def yield_paths(self, paths):
         # Yield ( <path>, <value-generator> ) tuples for all specified paths
@@ -249,42 +309,54 @@ class Parser:
         # Track the indexes of the paths in paths to be yielded.
         unyielded_path_idxs = set(range(len(paths)))
         path = []
-        for type_value in self.parse():
-            if isinstance(type_value, tuple):
-                _type, value = type_value
+        for event_value in self.parse():
+            if isinstance(event_value, tuple):
+                event, value = event_value
             else:
-                _type, value  = type_value, None
-            if _type == 'OBJECT_OPEN':
+                event, value  = event_value, None
+
+            if event == Events.OBJECT_OPEN:
                 if path and isinstance(path[-1], int):
                     path[-1] += 1
                 path.append(b'.')
                 continue
-            elif _type == 'OBJECT_CLOSE':
+
+            elif event == Events.OBJECT_CLOSE:
                 path.pop()
                 continue
-            elif _type == 'ARRAY_OPEN':
+
+            elif event == Events.ARRAY_OPEN:
                 if path and isinstance(path[-1], int):
                     path[-1] += 1
                 path.append(-1)
                 continue
-            elif _type == 'ARRAY_CLOSE':
+
+            elif event == Events.ARRAY_CLOSE:
                 path.pop()
                 continue
-            elif _type == 'OBJECT_KEY':
+
+            elif event == Events.OBJECT_KEY:
                 path[-1] = b''.join(value)
-            elif _type.startswith('ARRAY_VALUE_'):
+
+            elif (event == Events.ARRAY_VALUE_STRING
+                  or event == Events.ARRAY_VALUE_NUMBER
+                  or event == Events.ARRAY_VALUE_NULL):
                 path[-1] += 1
                 for i in unyielded_path_idxs:
                     if path == paths[i]:
-                        yield path, self.convert(_type, value)
+                        yield path, self.convert(event, value)
                         unyielded_path_idxs.remove(i)
                         break
-            elif _type.startswith('OBJECT_VALUE_'):
+
+            elif (event == Events.OBJECT_VALUE_STRING
+                  or event == Events.OBJECT_VALUE_NUMBER
+                  or event == Events.OBJECT_VALUE_NULL):
                 for i in unyielded_path_idxs:
                     if path == paths[i]:
-                        yield path, self.convert(_type, value)
+                        yield path, self.convert(event, value)
                         unyielded_path_idxs.remove(i)
                         break
+
             # Abort if all of the requested paths have been yielded.
             if len(unyielded_path_idxs) == 0:
                 return
@@ -292,20 +364,22 @@ class Parser:
     def load(self):
         parse_gen = self.parse()
         # Initialize the value based on the first read.
-        type_value = next(parse_gen)
-        _type, value = (type_value if isinstance(type_value, tuple)
-                        else (type_value, None))
+        event_value = next(parse_gen)
+        event, value = (event_value if isinstance(event_value, tuple)
+                        else (event_value, None))
         # If it's a single scalar value, convert and return it.
-        if _type == 'STRING' or _type == 'NUMBER' or _type == 'NULL':
-            return self.convert(_type, value)
+        if (event == Events.STRING
+            or event == Events.NUMBER
+            or event == Events.NULL):
+            return self.convert(event, value)
 
         # Read the initial container type.
-        if _type == 'OBJECT_OPEN':
+        if event == Events.OBJECT_OPEN:
             root = {}
-        elif _type == 'ARRAY_OPEN':
+        elif event == Events.ARRAY_OPEN:
             root = []
         else:
-            raise NotImplementedError(_type)
+            raise NotImplementedError(event)
 
         # Continue parsing.
         container_stack = []
@@ -328,49 +402,27 @@ class Parser:
             nonlocal container
             container = container_stack.pop()
 
-        for type_value in parse_gen:
-            _type, value = (type_value if isinstance(type_value, tuple)
-                            else (type_value, None))
-            if _type == 'ARRAY_OPEN':
+        for event_value in parse_gen:
+            event, value = (event_value if isinstance(event_value, tuple)
+                            else (event_value, None))
+            if event == Events.ARRAY_OPEN:
                 open_container([])
-            elif _type == 'OBJECT_OPEN':
+            elif event == Events.OBJECT_OPEN:
                 open_container({})
-            elif _type == 'ARRAY_CLOSE' or _type == 'OBJECT_CLOSE':
+            elif event == Events.ARRAY_CLOSE or event == Events.OBJECT_CLOSE:
                 if len(container_stack) == 0:
                     # No more open containers, so stop parsing.
                     break
                 close_container()
-            elif _type.startswith('ARRAY_VALUE_'):
-                container.append(self.convert(_type, value))
-            elif _type == 'OBJECT_KEY':
-                key = self.convert(_type, value)
-            elif _type.startswith('OBJECT_VALUE_'):
-                container[key] = self.convert(_type, value)
+            elif (event == Events.ARRAY_VALUE_STRING
+                  or event == Events.ARRAY_VALUE_NUMBER
+                  or event == Events.ARRAY_VALUE_NULL):
+                container.append(self.convert(event, value))
+            elif event == Events.OBJECT_KEY:
+                key = self.convert(event, value)
+            elif (event == Events.OBJECT_VALUE_STRING
+                  or event == Events.OBJECT_VALUE_NUMBER
+                  or event == Events.OBJECT_VALUE_NULL):
+                container[key] = self.convert(event, value)
 
         return root
-
-# TEST
-if __name__ == '__main__':
-    fh = open("test_inputs/api_weather_gov_points.json", "rb")
-
-    # DEBUG
-    for path, value in Parser(fh).yield_paths((
-            [b'@context', 1, b'unitCode', b'@type'],
-            [b'properties', b'relativeLocation', b'geometry', b'coordinates', 0 ],
-
-    )):
-        print(path, value)
-    exit(0)
-
-    parser = Parser(fh)
-    for type_value in parser.parse():
-        if not isinstance(type_value, tuple):
-            print(type_value)
-            continue
-        _type, value = type_value
-        if _type == 'OBJECT_KEY' or _type.endswith('STRING'):
-            print(_type, '"{}"'.format(''.join(value)))
-        elif _type.endswith('NUMBER'):
-            print(_type, '{}'.format(float(''.join(value))))
-        else:
-            raise NotImplementedError
