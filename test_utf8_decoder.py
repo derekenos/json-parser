5adbcbc55590b6bfa887baa8a95c481b0a723b1e
Derek Enos <derek.enos@gmail.com>
2020-12-18 15:13:33 -0500

    Removed byte order mark stuff and added UTF8 stress test file, which doesn't pass.

---
+++ test_utf8_decoder.py
@@ -0,0 +1,65 @@
+
+from io import BytesIO
+from sys import stdout
+
+from utf8_decoder import (
+    InvalidUTF8Encoding,
+    UTF8Decoder,
+)
+
+###############################################################################
+# Testing helpers
+###############################################################################
+
+class Skip(Exception): pass
+class DidNotRaise(Exception): pass
+
+def _assertEqual(result, expected):
+    if result != expected:
+        raise AssertionError(
+            f'Expected ({repr(expected)}), got ({repr(result)})'
+        )
+
+def _assertRaises(exc, fn, *args, **kwargs):
+    try:
+        fn(*args, **kwargs)
+    except exc:
+        pass
+    else:
+        raise DidNotRaise
+
+###############################################################################
+# Tests
+###############################################################################
+
+def test_stress_test():
+    fh = open('UTF-8-test.txt', 'rb')
+    decoder = UTF8Decoder(fh)
+    for c in decoder:
+        stdout.write(c)
+
+
+###############################################################################
+
+def run_tests():
+    # Run all global functions with a name that starts with "test_".
+    fn_cls = type(run_tests)
+    for k, v in sorted(globals().items()):
+        if k.startswith('test_') and isinstance(v, fn_cls):
+            test_name = v.__name__[5:]
+            stdout.write('testing {}'.format(test_name))
+            stdout.flush()
+            try:
+                v()
+            except AssertionError as e:
+                stdout.write(' - FAILED\n')
+                raise
+            except Skip:
+                stdout.write(' - SKIPPED\n')
+            else:
+                stdout.write(' - ok\n')
+            finally:
+                stdout.flush()
+
+if __name__ == '__main__':
+    run_tests()
--- utf8_decoder.py
+++ utf8_decoder.py
@@ -7,21 +7,24 @@ class InvalidUTF8Encoding(Exception):
     def __init__(self, byte_num):
         super().__init__(
             self,
-            'Invalid UTF-8 encoding at char: {}'.format(char_num)
+            'Invalid UTF-8 encoding at byte number: {}'.format(byte_num)
         )
 
 class UTF8Decoder:
     def __init__(self, stream):
         self.stream = stream
         self.byte_num = 0
-        self.checked_type = False
+        self.first_read = True
 
     def read_one(self):
-        self.byte_num += 1
         c = self.stream.read(1)
-        if not self.checked_type:
+        self.byte_num += 1
+
+        if self.first_read:
             if not isinstance(c, bytes):
                 raise AssertionError('UTF8Decoder requires a bytes stream')
+            self.first_read = False
+
         if c == b'':
             raise StopIteration
         return c
@@ -34,27 +37,44 @@ class UTF8Decoder:
         # Read the next char.
         byte = ord(self.read_one())
         # If the high bit is clear, return the single-byte char.
-        if byte & 0x80 == 0:
+        if byte & 0b10000000 == 0:
             return chr(byte)
         # The high bit is set so char comprises multiple bytes.
-        # Determine the number of bytes and do the read.
-        if byte & 0x11100000 == 0x11000000:
+        # Determine the number of bytes and init the codepoint with
+        # the first byte.
+        if byte & 0b11100000 == 0b11000000:
             # 2-byte char.
             bytes_remaining = 1
-        elif byte & 0x11110000 == 0x11100000:
+            codepoint = (byte & 0b00011111) << 6
+        elif byte & 0b11110000 == 0b11100000:
             # 3-byte char.
             bytes_remaining = 2
-        else:
+            codepoint = (byte & 0b00001111) << 12
+        elif byte & 0b11111000 == 0b11110000:
+            # 4-byte char.
             bytes_remaining = 3
-        # Init the code point value with the appropriately shifted
-        # first byte.
-        codepoint = (byte & 0x00011111) << (6 * bytes_remaining)
+            codepoint = (byte & 0b00000111) << 18
+        elif byte & 0b11111100 == 0b11111000:
+            # 5-byte char.
+            bytes_remaining = 4
+            codepoint = (byte & 0b00000011) << 24
+        elif byte & 0b11111110 == 0b11111100:
+            # 6-byte char.
+            bytes_remaining = 5
+            codepoint = (byte & 0b00000001) << 30
+        else:
+            raise InvalidUTF8Encoding(self.byte_num)
+
         # Read the remaining bytes, asserting that they're valid,
         # then shifting and ORing them with the codepoint.
         while bytes_remaining:
-            byte = self.read_one()
+            try:
+                byte = ord(self.read_one())
+            except StopIteration:
+                # Stream exhausted in the middle of a multi-byte char.
+                raise InvalidUTF8Encoding(self.byte_num)
             if byte & 0b11000000 != 0b10000000:
-                raise InvalidUTF8Encoding(self.char_num)
-            codepoint |= (byte & 0b00111111) << (bytes_remaining - 1)
+                raise InvalidUTF8Encoding(self.byte_num)
+            codepoint |= ((byte & 0b00111111) << ((bytes_remaining - 1) * 6))
             bytes_remaining -= 1
         return chr(codepoint)
