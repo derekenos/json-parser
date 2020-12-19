fce2958588e3efd0071e5932982b1cd932053856
Derek Enos <derek.enos@gmail.com>
2020-12-19 14:54:58 -0500

    Moved utf-8 decoder into its own dir and added another test - which fails.

--- utf8_decoder/test.py
+++ utf8_decoder/test.py
@@ -34,11 +34,22 @@ def _assertRaises(exc, fn, *args, **kwargs):
 ###############################################################################
 
 def test_stress_test():
-    fh = open('UTF-8-test.txt', 'rb')
-    decoder = UTF8Decoder(fh, errors=utf8_decoder.REPLACE)
+    # Test processing the UTF-8 decoder capability and stress test from:
+    # https://www.cl.cam.ac.uk/~mgk25/ucs/examples/UTF-8-test.txt
+    input_fh = open('test_data/UTF-8-test.txt', 'rb')
+    expected_fh = open('test_data/UTF-8-test.txt.expected', 'r', encoding='utf-8')
+    decoder = UTF8Decoder(input_fh, errors=utf8_decoder.REPLACE)
     for c in decoder:
-        stdout.write(c)
+        _assertEqual(c, expected_fh.read(1))
 
+def test_i_can_eat_glass():
+    # Test processing the "I CAN EAT GLASS" section at:
+    # http://kermitproject.org/utf8.html#glass
+    original_fh = open('test_data/i_can_eat_glass.txt', 'r', encoding='utf-8')
+    decoder_fh = open('test_data/i_can_eat_glass.txt', 'rb')
+    decoder = UTF8Decoder(decoder_fh, errors=utf8_decoder.REPLACE)
+    for c in decoder:
+        _assertEqual(c, original_fh.read(1))
 
 ###############################################################################
 
